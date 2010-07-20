05db8d13408aac6a9c861224db7c007998832ee1
Andreas Tolf Tolfsen <andreastt@opera.com>
2010-07-20 13:31:21 +0200

    Adding watirspec.

---
+++ spec/spec_helper.rb
@@ -0,0 +1,12 @@
+$LOAD_PATH.unshift("watirspec/lib")
+
+require "rubygems"
+require "operawatir"
+#require "watirspec/spec_helper"
+
+Browser = OperaWatir::Browser
+
+#WatirSpec.browser_args = ["-no-window"]
+#WatirSpec.persistent_browser = false
+#WatirSpec::Server.autorun = true
+
