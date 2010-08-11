038c93fb8eada6d1676b831ffb428022ccd8c307
Andreas Tolf Tolfsen <andreastt@opera.com>
2010-08-11 09:58:23 +0200

    Added OperaHelper.

--- test/browser.rb
+++ test/browser.rb
@@ -4,28 +4,30 @@ describe "browser" do
   # back
   it "should go back" do
     puts "test 1"
-    @browser.goto @url + "foo.html"
-    @browser.goto @url + "bar.html"
-    @browser.back
-    @browser.text.include?("foo").should == true
+#    puts "files: " + files
+    browser.goto files + "/foo.html"
+#    puts "  after first browser call"
+    browser.goto files + "/bar.html"
+    browser.back
+    browser.text.include?("foo").should == true
   end
-
+=begin
   # goto
   it "should load HTML page" do
     puts "test 2"
-    @browser.goto @url + "blank.html"
+    browser.goto files + "/blank.html"
   end
 
   it "should load about:blank" do
     puts "test 3"
-    @browser.goto "about:blank"
-    @browser.text.size.should == 0
+    browser.goto "about:blank"
+    browser.text.size.should == 0
   end
 
   # is_connected
   it "should be connected to a browser instance" do
     puts "test 4"
-    @browser.is_connected?.should == true
+    browser.is_connected?.should == true
   end
 
 #  it "should not be connected to a browser instance" do
@@ -35,12 +37,13 @@ describe "browser" do
 
   it "should be connected to a browser instance after starting a new instance" do
     puts "test 5"
-#    @browser.is_connected?.should == true
+    browser.is_connected?.should == true
   end
 
   it "foobar" do
     puts "test 6"
-    @browser.goto "http://sny.no/"
+    browser.goto "http://sny.no/"
   end
+=end
 end
 
--- test/p/actions.rb
+++ test/p/actions.rb
@@ -1,28 +1,35 @@
 require "operahelper"
 
 describe "p element actions" do
+  before(:each) do
+    puts "path: " + files + "/p/exists.html"
+  end
+
   it "should be enabled" do
-    @browser.goto @url + "foobar.html"
-    @browser.p(:xpath, "//p[1]").enabled?.should == true
+    pending "foo"
+    browser.goto files + "/foobar.html"
+    browser.p(:xpath, "//p[1]").enabled?.should == true
   end
 
   it "should exist" do
-    @browser.goto @url + "p/exists.html"
-    @browser.p(:id, "one").exists?.should == true
-    @browser.p(:id, "two").exists?.should == true
-    @browser.p(:id, "four").exists?.should == true
-    @browser.p(:id, "five").exists?.should == true
+    browser.goto files + "/p/exists.html"
+    browser.p(:id, "one").exists?.should == true
+    browser.p(:id, "two").exists?.should == true
+    browser.p(:id, "four").exists?.should == true
+    browser.p(:id, "five").exists?.should == true
+
+    sleep(10)
   end
 
   it "should not exist" do
-    @browser.goto @url + "p/exists.html"
-    @browser.p(:id, "three").exists?.should == false
+    browser.goto files + "/p/exists.html"
+    browser.p(:id, "three").exists?.should == false
   end
 
   it "should not be able to clear innerHTML" do
-    @browser.goto @url + "foobar.html"
-    @browser.p(:xpath, "//p[1]").clear
-    @browser.p(:xpath, "//p[1]").text.include?("foobar").should == true
+    browser.goto files + "/foobar.html"
+    browser.p(:xpath, "//p[1]").clear
+    browser.p(:xpath, "//p[1]").text.include?("foobar").should == true
   end
 end
 
