037548ca829192f4f0d03194f0f7ad4b0a3f051e
Andreas Tolf Tolfsen <andreastt@opera.com>
2010-08-11 09:58:23 +0200

    Added OperaHelper.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,4 @@
-$LOAD_PATH.unshift("watirspec/lib")
+#$LOAD_PATH.unshift("watirspec/lib")
 
 require "rubygems"
 require "operawatir"
---
+++ test/helper.rb
@@ -0,0 +1,8 @@
+#OperaWatir::Helper.files = "/path/to/somewhere"
+#OperaWatir::Helper.persistent_browser = false
+
+OperaWatir::Helper.browser_args =
+  ["/home/andreastt/gogilauncher/builds/opera-ppp-mainline-build_4342-buildsetid_24137-core_2_6-lingogi/profiles/release_desktop/lingogi_release_desktop"]
+#    "-nowindow"]
+#OperaWatir::helper.inspectr = true
+
