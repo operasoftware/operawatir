9f1b79458c72fdaeb39a8a76ee98bd68f4951f7f
Chris Lloyd <christopherl@opera.com>
2010-08-27 15:36:23 +0200

    Another refactor.
     * Removed alot of comments. RDoc sucks and it's getting in the way for the moment. Can merge back in later if they are severely missed.
     * Moved Container into Elements. Makes more sense.
     * Deals with default arguments better.

--- lib/operawatir/elements/text_field.rb
+++ lib/operawatir/elements/text_field.rb
@@ -1,28 +1,11 @@
 module OperaWatir
   class TextField < WebElement
 
-    # Sets the contents of the text field to the provided text.
-    # Will overwrite the existing contents.
-    #
-    # Input:
-    # value::  Text to be set.
-    #
-    # Raises:
-    # ObjectDisabledException::  if text field is disabled.
-    # ObjectReadOnlyException::  if text field is read only.
     def set(text)
       clear unless value.empty?
       element.sendKeys(text.split(//).to_java(:string))
     end
 
-    # Append the provided text to the contents of the text field.
-    #
-    # Input:
-    # value::  Text to be appended.
-    #
-    # Raises:
-    # ObjectDisabledException::  if text field is disabled.
-    # ObjectReadOnlyException::  if text field is read only.
     def append(text)
       set(value + text)
     end
