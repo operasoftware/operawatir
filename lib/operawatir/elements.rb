46606333f71d36ca16cb3f14050f5b28a8b5e098
Andreas Tolf Tolfsen <andreastt@opera.com>
2010-08-27 18:26:36 +0200

    File field methods are now in a separate file.

--- lib/operawatir/elements.rb
+++ lib/operawatir/elements.rb
@@ -1,3 +1,3 @@
 %w(web_element non_control_elements button checkbox form image link
-   radio select_list text_field).each { |elm| require "operawatir/elements/#{elm}" }
+   radio select_list text_field file_field).each { |elm| require "operawatir/elements/#{elm}" }
 
--- lib/operawatir/elements/text_field.rb
+++ lib/operawatir/elements/text_field.rb
@@ -27,19 +27,5 @@ module OperaWatir
       set(value + text)
     end
   end
-
-  class FileField < TextField
-
-    # Sets the path of the file in the textbox.  Path provided must be
-    # an absolute path.
-    #
-    # Input:
-    # value::  Absolute path of the file.
-    def set(path)
-      clear unless value.empty?
-      element.click 1, 0
-      element.sendKeys(path.split(//).to_java(:string))
-    end
-  end
 end
 
