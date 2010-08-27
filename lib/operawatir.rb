1b83136f3981851855f559ad786673e5ae151fd0
Andreas Tolf Tolfsen <andreastt@opera.com>
2010-08-27 16:53:09 +0200

    - Added a new webdriver jar. - Fixed line breaks, new lines, &c. - Updated submodule. - Added method for retrieving OperaDriver version number.

--- lib/operawatir.rb
+++ lib/operawatir.rb
@@ -1,8 +1,8 @@
-require 'java'
+require "java"
 include Java
 
 module OperaWatir
-  VERSION = File.read(File.join(File.dirname(__FILE__), '..', 'VERSION')).strip
+  VERSION = File.read(File.join(File.dirname(__FILE__), "..", "VERSION")).strip
 
   def self.version
     VERSION
@@ -11,7 +11,7 @@ end
 
 %w(commons-jxpath-1.3.jar protobuf-java-2.3.0.jar webdriver-common.jar
    winp-1.14.jar webdriver-opera.jar).
-   each {|jar| require "operadriver/#{jar}"}
+   each { |jar| require "operadriver/#{jar}" }
 
 include_class org.openqa.selenium.WebDriver
 include_class org.openqa.selenium.RenderedWebElement
@@ -19,6 +19,7 @@ include_class org.openqa.selenium.NoSuchElementException
 include_class com.opera.core.systems.OperaDriver
 include_class com.opera.core.systems.OperaWebElement
 
-require 'operawatir/container'
-require 'operawatir/browser'
-require 'operawatir/elements'
+require "operawatir/container"
+require "operawatir/browser"
+require "operawatir/elements"
+
