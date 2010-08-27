7aec56bfcd3566682568915187b81520b3e0a636
Chris Lloyd <christopherl@opera.com>
2010-08-27 16:02:15 +0200

    Fixing some exceptions, adding some attribute convenience methods.

--- lib/operawatir/elements/link.rb
+++ lib/operawatir/elements/link.rb
@@ -1,4 +1,14 @@
 module OperaWatir
   class Link < WebElement
+
+    def self.default_method
+      :href
+    end
+
+    def href
+      get_attribute('href')
+    end
+    alias_method :url, :href
+
   end
 end
