7886fc3f3409c6b58f2460d8b5a397d6969218b8
Chris Lloyd <christopherl@opera.com>
2010-08-27 15:36:23 +0200

    Another refactor.
     * Removed alot of comments. RDoc sucks and it's getting in the way for the moment. Can merge back in later if they are severely missed.
     * Moved Container into Elements. Makes more sense.
     * Deals with default arguments better.

--- lib/operawatir/elements/checkbox.rb
+++ lib/operawatir/elements/checkbox.rb
@@ -1,26 +1,12 @@
 module OperaWatir
   class CheckBox < WebElement
 
-    # Checks or unchecks the checkbox.  If no value is supplied it will
-    # check the checkbox.
-    #
-    # Input:
-    # value::  Parameter indicated whether to check or uncheck the checkbox.  True to check the check box, false for unchecking the checkbox.
-    #
-    # Raises:
-    # NoSuchElementException::   if the element is not found.
-    # ObjectDisabledException::  if the element is disabled.
     def set(value = true)
       until checked? == value
         element.toggle
       end
     end
 
-    # Determines if a check box is set.  Returns true if set/checked;
-    # false if not set/checked.
-    #
-    # Raises:
-    # NoSuchElementException::  if the element is not found.
     def checked?
       element.isSelected
     end
