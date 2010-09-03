e1ea15e4036fdb3619c002aedff7a9ebfcdd03f4
Chris Lloyd <christopherl@opera.com>
2010-09-03 13:52:16 +0200

    Lots of new elements and stubs for collections.

---
+++ lib/operawatir/collections.rb
@@ -0,0 +1,44 @@
+module OperaWatir
+  module Collections
+
+    def rows
+    end
+
+    def ols
+    end
+
+    def file_fields
+    end
+
+    def divs
+    end
+
+    def buttons
+    end
+
+    def frames
+    end
+
+    def tfoots
+    end
+
+    def bodies
+    end
+
+    def radios
+    end
+
+    def dels
+    end
+
+    def ems
+    end
+
+    def h1s
+    end
+
+    def h2s
+    end
+
+  end
+end
