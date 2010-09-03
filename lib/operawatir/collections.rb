98f0f2cecc64dc7dbe35162f4609acb203b81343
Chris Lloyd <christopherl@opera.com>
2010-09-03 15:05:23 +0200

    Add/stub all elements.

--- lib/operawatir/collections.rb
+++ lib/operawatir/collections.rb
@@ -1,43 +1,151 @@
 module OperaWatir
   module Collections
 
-    def rows
+    def self.included(klass)
+      klass.extend
     end
 
-    def ols
+    module InstanceMethods
+
+      def collection_accessor(collections={})
+        collections.each do |kl, klass|
+          define_method(name) do
+            @driver.getElementsByTagName(name.to_s).map do |elm|
+              klass.new_with_element self, elm
+            end
+          end
+        end
+      end
+
     end
 
-    def file_fields
-    end
-
-    def divs
+    def areas
     end
 
     def buttons
     end
 
-    def frames
+    def checkboxes
     end
 
-    def tfoots
-    end
-
-    def bodies
-    end
-
-    def radios
+    def dds
     end
 
     def dels
     end
 
+    def divs
+    end
+
+    def dls
+    end
+
+    def dts
+    end
+
+    def elements
+    end
+
     def ems
+    end
+
+    def file_fields
+    end
+
+    def forms
+    end
+
+    def frames
+    end
+
+    def hiddens
     end
 
     def h1s
     end
 
     def h2s
+    end
+
+    def h3s
+    end
+
+    def h4s
+    end
+
+    def h5s
+    end
+
+    def h6s
+    end
+
+    def images
+    end
+
+    def inss
+    end
+
+    def labels
+    end
+
+    def links
+    end
+
+    def lis
+    end
+
+    def maps
+    end
+
+    def metas
+    end
+
+    def ols
+    end
+
+    def options
+    end
+
+    def pres
+    end
+
+    def ps
+    end
+
+    def radios
+    end
+
+    def select_lists
+    end
+
+    def spans
+    end
+
+    def strongs
+    end
+
+    def tbodys
+    end
+
+    def cells
+    end
+
+    def tfoots
+    end
+
+    def theads
+    end
+
+    def rows
+    end
+
+    def tables
+    end
+
+    def text_fields
+    end
+
+    def uls
     end
 
   end
--- lib/operawatir/elements.rb
+++ lib/operawatir/elements.rb
@@ -5,117 +5,178 @@
 module OperaWatir
   module Elements
 
-    def text_field(method=nil, selector=nil, value=nil)
-     TextField.new self, method, selector, value
-    end
+    # TODO Dry
 
-    def form(method=nil, selector=nil, value=nil)
-     Form.new self, method, selector, value
+    def area(mehtod=nil, selector=nil, value=nil)
+      Area.new self, method, selector, value
     end
 
     def button(method=nil, selector=nil, value=nil)
      Button.new self, method, selector, value
     end
 
-    def link(method=nil, selector=nil, value=nil)
-     Link.new self, method, selector, value
-    end
-
-    def select_list(method=nil, selector=nil, value=nil)
-     SelectList.new self, method, selector, value
-    end
-
     def checkbox(method=nil, selector=nil, value=nil)
      CheckBox.new self, method, selector, value
     end
 
-    def radio(method=nil, selector=nil, value=nil)
-     Radio.new self, method, selector, value
+    def dd(method=nil, selector=nil, value=nil)
+      Dd.new self, method, selector, value
     end
 
-    def area(method=nil, selector=nil, value=nil)
-     Area.new self, method, selector, value
-    end
-
-    def image(method=nil, selector=nil, value=nil)
-     Image.new self, method, selector, value
+    def del(method=nil, selector=nil, value=nil)
+      Del.new self, method, selector, value
     end
 
     def div(method=nil, selector=nil, value=nil)
      Div.new self, method, selector, value
     end
 
-    def p(method=nil, selector=nil, value=nil)
-     P.new self, method, selector, value
+    def dl(method=nil, selector=nil, value=nil)
+      Dl.new self, method, selector, value
+    end
+
+    def dt(method=nil, selector=nil, value=nil)
+      Dt.new self, method, selector, value
     end
 
     def element(method=nil, selector=nil, value=nil)
      Element.new self, method, selector, value
     end
 
-    def hidden(method=nil, selector=nil, value=nil)
-     Hidden.new self, method, selector, value
+    def em(method=nil, selector=nil, value=nil)
+     Em.new self, method, selector, value
     end
 
     def file_field(method=nil, selector=nil, value=nil)
      FileField.new self, method, selector, value
     end
 
-    def ul(method=nil, selector=nil, value=nil)
-     Ul.new self, method, selector, value
+    def form(method=nil, selector=nil, value=nil)
+     Form.new self, method, selector, value
+    end
+
+    def frame
+      raise 'TODO'
+    end
+
+    def hidden(method=nil, selector=nil, value=nil)
+     Hidden.new self, method, selector, value
+    end
+
+    def h1(method=nil, selector=nil, value=nil)
+     H1.new self, method, selector, value
+    end
+
+    def h2(method=nil, selector=nil, value=nil)
+     H2.new self, method, selector, value
+    end
+
+    def h3(method=nil, selector=nil, value=nil)
+     H3.new self, method, selector, value
+    end
+
+    def h4(method=nil, selector=nil, value=nil)
+     H4.new self, method, selector, value
+    end
+
+    def h5(method=nil, selector=nil, value=nil)
+     H5.new self, method, selector, value
+    end
+
+    def h6(method=nil, selector=nil, value=nil)
+     H6.new self, method, selector, value
+    end
+
+    def image(method=nil, selector=nil, value=nil)
+     Image.new self, method, selector, value
+    end
+
+    def ins(method=nil, selector=nil, value=nil)
+     Ins.new self, method, selector, value
+    end
+
+    def label(method=nil, selector=nil, value=nil)
+     Label.new self, method, selector, value
+    end
+
+    def link(method=nil, selector=nil, value=nil)
+     Link.new self, method, selector, value
+    end
+
+    def li(method=nil, selector=nil, value=nil)
+      Li.new self, method, selector, value
+    end
+
+    def map(method=nil, selector=nil, value=nil)
+      Map.new self, method, selector, value
+    end
+
+    def meta(method=nil, selector=nil, value=nil)
+      Meta.new self, method, selector, value
+    end
+
+    def ol(method=nil, selector=nil, value=nil)
+     Ol.new self, method, selector, value
+    end
+
+    def option(method=nil, selector=nil, value=nil)
+     Option.new self, method, selector, value
+    end
+
+    def pre(method=nil, selector=nil, value=nil)
+     Pre.new self, method, selector, value
+    end
+
+    def p(method=nil, selector=nil, value=nil)
+     P.new self, method, selector, value
+    end
+
+    def radio(method=nil, selector=nil, value=nil)
+     Radio.new self, method, selector, value
+    end
+
+    def select_list(method=nil, selector=nil, value=nil)
+     SelectList.new self, method, selector, value
     end
 
     def span(method=nil, selector=nil, value=nil)
      Span.new self, method, selector, value
     end
 
-    def em(method=nil, selector=nil, value=nil)
-     Em.new self, method, selector, value
+    def strong(method=nil, selector=nil, value=nil)
+     Strong.new self, method, selector, value
     end
 
-    def dl(method=nil, selector=nil, value=nil)
-      Dl.new self, method, selector, value
+    def tbody(method=nil, selector=nil, value=nil)
+      TableBody.new self, method, selector, value
     end
 
-    # TODO
-    def option
-      Option.new self, method, selector, value
-    end
-
-    def meta
-      Meta.new self, method, selector, value
-    end
-
-    def li
-      Li.new self, method, selector, value
-    end
-
-    def table
-      Table.new self, method, selector, value
-    end
-
-    def tfoot
-      TableFooter.new self, method, selector, value
-    end
-
-    def dd
-      Dd.new self, method, selector, value
-    end
-
-    def cell
+    def cell(method=nil, selector=nil, value=nil)
       Cell.new self, method, selector, value
     end
 
-    def map
-      Map.new self, method, selector, value
+    def tfoot(method=nil, selector=nil, value=nil)
+      TableFooter.new self, method, selector, value
     end
 
-    def pre
-      Pre.new self, method, selector, value
+    def thead(method=nil, selector=nil, value=nil)
+      TableHeader.new self, method, selector, value
     end
 
-    def del
-      Del.new self, method, selector, value
+    def row(method=nil, selector=nil, value=nil)
+      Row.new self, method, selector, value
+    end
+
+    def table(method=nil, selector=nil, value=nil)
+      Table.new self, method, selector, value
+    end
+
+    def text_field(method=nil, selector=nil, value=nil)
+     TextField.new self, method, selector, value
+    end
+
+    def ul(method=nil, selector=nil, value=nil)
+     Ul.new self, method, selector, value
     end
 
   end
