2d7504c749a4535d44b2d513e6982073847bcee6
Chris Lloyd <christopherl@opera.com>
2010-09-03 10:38:37 +0200

    Add elements back in.

--- lib/operawatir/elements.rb
+++ lib/operawatir/elements.rb
@@ -1,3 +1,3 @@
-%w(web_element non_control_elements button checkbox form image link
+%w(web_element non_control_elements button checkbox element form image link
    radio select_list text_field file_field).each { |elm| require "operawatir/elements/#{elm}" }
 
