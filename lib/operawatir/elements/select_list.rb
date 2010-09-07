8aa6bd7254e464a0635a769f15b014a2da8f514b
Chris Lloyd <christopherl@opera.com>
2010-09-07 17:07:57 +0200

    Refactor SelectList and add Hidden class.

--- lib/operawatir/elements/select_list.rb
+++ lib/operawatir/elements/select_list.rb
@@ -2,18 +2,14 @@ module OperaWatir
   class SelectList < WebElement
 
     def select(value)
-      find_option_by_text(items, value).setSelected
+      find_option_by_val(value).setSelected
     end
-    alias_method :set, :select
 
-    def select_value(value)
-      element.findElementsByTagName("option")
-      find_option_by_val(items, value).setSelected
-    end
+    alias_method :set, :select
+    alias_method :select_value, :select
 
     def selected?(value)
-      element.findElementsByTagName("option")
-      find_option_by_val(items, value).isSelected
+      find_option_by_val(value).isSelected
     end
 
     def options
@@ -26,14 +22,14 @@ module OperaWatir
 
   private
 
-    def find_option_by_val(items, value)
-      options.find {|item| item.getValue == value } ||
+    def find_option_by_val(value)
+      options.find {|option| option.getValue == value } ||
         raise(Exceptions::UnknownObjectException,
               "Unable to locate Option, using :value and #{value.inspect}")
     end
 
-    def find_option_by_text(items, value)
-      options.find {|item| item.getText == value} ||
+    def find_option_by_text(value)
+      options.find {|option| option.getText == value} ||
         raise(Exceptions::UnknownObjectException,
               "Unable to locate Option, using :text and #{value.inspect}")
     end
