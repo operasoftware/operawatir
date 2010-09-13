2def4818a110a5241246755d53e46986c37ba635
Adam Minchinton <adamm@opera.com>
2010-09-13 08:59:01 +0200

    Updates to make the different widgets work

--- lib/operawatir/desktop_container.rb
+++ lib/operawatir/desktop_container.rb
@@ -1,14 +1,32 @@
 module OperaWatir
   module DesktopContainer
+    def quick_button(how, what)
+      QuickButton.new(self, how, what)
+    end
+
+    def quick_checkbox(how, what)
+      QuickCheckbox.new(self, how, what)
+    end
+
+    def quick_dialog_tab(how, what)
+      QuickDialogTab.new(self, how, what)
+    end
+
+    def quick_dropdown(how, what)
+      QuickDropdown.new(self, how, what)
+    end
+
+    def quick_editfield(how, what)
+      QuickEditField.new(self, how, what)
+    end
 
     def quick_label(how,what)
       QuickLabel.new(self,how,what)
     end
 
-    def quick_button(how, what)
-      QuickButton.new(self, how, what)
+    def quick_radiobutton(how, what)
+      QuickRadioButton.new(self, how, what)
     end
-
   end
 end
 
