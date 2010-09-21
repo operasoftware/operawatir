bce98723c84139abe7858fed2662fcc263c97f88
Chris Lloyd <christopherl@opera.com>
2010-09-21 13:34:14 +0200

    Use attr_reader for Brower@driver.

--- lib/operawatir/browser.rb
+++ lib/operawatir/browser.rb
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 module OperaWatir
   class Browser
     include Elements
@@ -5,6 +6,12 @@ module OperaWatir
 
     attr_reader :driver
     alias_method :element, :driver
+    
+    def self.start(url, *args)
+      b = new(*args)
+      b.goto url
+      b
+    end
 
     def initialize(executable_location = nil, *arguments)
       if executable_location.nil?
@@ -18,41 +25,45 @@ module OperaWatir
     # Loads the given URL in the browser.  Waits for the page to get loaded.
     def goto(url = "")
       raise 'You need to specify an address' if url.empty?
-      @driver.get(url)
+      driver.get(url)
     end
 
     # Stops the loading of the current page.
     def stop
-      @driver.stop
+      driver.stop
     end
 
     # Cleans up and close the connection to the browser instance.  Will
     # not terminate the browser program.
     def clean_up
-      @driver.cleanUp
+      driver.cleanUp
     end
 
     # Returns the title of the page.
     def title
-      @driver.getTitle
+      driver.getTitle
     end
 
     # Closes the open page/tab.
     def close
-      @driver.close
-    end
+      driver.close
+  end
 
     # Returns the full text of the active page.
     def text
-      @driver.getText
+      driver.getText
     end
 
     def html
-      @driver.getPageSource
+      driver.getPageSource
+    end
+
+    def exist?
+      url != ''
     end
 
     # TODO
-    def contains_text?
+    def contains_text?(a)
     end
 
     alias_method :contains_text, :contains_text?
@@ -62,22 +73,22 @@ module OperaWatir
     end
 
     def element_by_xpath(selector)
-      @driver.findElementByXPath(selector)
+      driver.findElementByXPath(selector)
     end
 
     # Instructs the browser to navigate one page backwards.
     def back
-      @driver.navigate.back
+      driver.navigate.back
     end
 
     # Instructs the browser to navigate one page forward.
     def forward
-      @driver.navigate.forward
+      driver.navigate.forward
     end
 
     # Refreshes the current page.
     def refresh
-      @driver.navigate.refresh
+      driver.navigate.refresh
     end
 
     # Switches focus (active page) to the specified frame.
@@ -88,21 +99,21 @@ module OperaWatir
     def frame(how, what)
       case how
       when :name
-        @driver.switchTo.frame(what)
+        driver.switchTo.frame(what)
       when :index
-        @driver.switchTo.frame(what.to_i - 1) #index starts from 1 in watir
+        driver.switchTo.frame(what.to_i - 1) #index starts from 1 in watir
       end
       self
     end
 
     # Switches focus for active page back to the default frame.
     def switch_to_default
-      @driver.switchTo.defaultContent
+      driver.switchTo.defaultContent
     end
 
     # Output a list of frames to the console.
     def show_frames
-      frames = @driver.listFrames
+      frames = driver.listFrames
       puts "There are #{frames.length.to_s} frames"
       frames.each_with_index { |frame,i|
         puts "frame index:#{(i.to_i+1).to_s} name:#{frame.to_s}"
@@ -112,41 +123,41 @@ module OperaWatir
     # Closes all open pages and quits the browser instance.  The browser
     # instance will be terminated.
     def close_all
-      @driver.quit
+      driver.quit
     end
     alias_method :quit, :close_all
 
     # Executes the given JavaScript string, and returns the result.
     def execute_script(source)
-      @driver.executeScript(source, [].to_java(:String))
+      driver.executeScript(source, [].to_java(:String))
     end
 
     # Send key events to the browser instance.  I.e. “Down” (arrow
     # down), “Space” (space key), “Home”, &c.
     def key(key)
-      @driver.key(key)
+      driver.key(key)
     end
 
     # Enables you to hold down a key, i.e. “Ctrl”, “Alt”, “Shift”, &c.
     # Remember to release the keys afterwards with the key_up method.
     def key_down(key)
-      @driver.keyDown(key)
+      driver.keyDown(key)
     end
 
     # Releases a held down key.
     def key_up(key)
-      @driver.keyUp(key)
+      driver.keyUp(key)
     end
 
     # Types given text directly in to the browser.  The text will be
     # inputted to the page depending on where the focus is.
     def type(text)
-      @driver.type(text)
+      driver.type(text)
     end
 
     # Return current URI.
     def url
-      @driver.getCurrentUrl
+      driver.getCurrentUrl
     end
 
     # Execute specified Opera action.
@@ -155,7 +166,7 @@ module OperaWatir
     # name::   name of the Opera action to be performed.
     # param::  (Optional.)  Optional parameter to be supplied with the Opera action.
     def opera_action(name, *param)
-      @driver.operaAction(name, param.to_java(:String))
+      driver.operaAction(name, param.to_java(:String))
     end
 
     # Takes screenshot of the entire page.
@@ -164,7 +175,7 @@ module OperaWatir
     # file_name::  the absolute file path you wish to save the screenshot to.
     # time_out::   will attempt to perform the action until the time out is reached.
     def take_screenshot(file_name,hashes,time_out)
-      @driver.saveScreenshot(file_name, time_out, hashes.to_java(:String))
+      driver.saveScreenshot(file_name, time_out, hashes.to_java(:String))
     end
 
     # Will return the hash of the visual representation of the entire
@@ -173,7 +184,7 @@ module OperaWatir
     # Arguments:
     # time_out::  Will attempt to get the hash of the page until the time out is reached.
     def get_hash(time_out = 50)
-      @driver.findElementByTagName('body').getImageHash
+      driver.findElementByTagName('body').getImageHash
     end
 
     # Returns a full list of available Opera actions in the Opera build
@@ -182,36 +193,36 @@ module OperaWatir
     # available to devices-type builds will vary greatly from those
     # available to desktop-types.
     def opera_action_list
-      @driver.getOperaActionList
+      driver.getOperaActionList
     end
 
     # Returns the number of open pages/tabs.
     def open_tabs
-      @driver.getWindowHandles.size
+      driver.getWindowHandles.size
     end
 
     # Will return the garbage collection.
     def gc
-      @driver.gc
+      driver.gc
     end
 
     # Checks if OperaWatir is connected to any browser instance.  Will
     # return true or false.
     def is_connected?
-      @driver.isConnected
+      driver.isConnected
     end
 
     # Returns the Process identifier (pid), a number used by Unix
     # kernels and Windows operating systems to identify a process.
     def pid
-      @driver.getPid
+      driver.getPid
     end
 
     # Returns the version number of OperaDriver.  Please note that the
     # version number of webdriver-opera _is not_ related to the version
     # number for OperaWatir.
     def version
-      @driver.getOperaDriverVersion
+      driver.getOperaDriverVersion
     end
 
   end
