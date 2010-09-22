39ca14b36cc752bbedaee15c39d94927430db26f
Karianne Ekern <karie@opera.com>
2010-09-22 09:58:15 +0200

    EditField handles both multilineedit and edit

--- lib/operawatir/quickwidgets/quick_editfield.rb
+++ lib/operawatir/quickwidgets/quick_editfield.rb
@@ -2,7 +2,14 @@ module OperaWatir
   class QuickEditField < QuickWidget
     def correct_type?
       #puts @element.getType
-      @element.getType == "multiline_edit"
+      @element.getType == "multiline_edit" || @element.getType == "edit"
     end
+    
+    #def set(text)
+       #clear unless value.empty?
+    #   element.click 1, 0
+    #   element.sendKeys(text.split(//).to_java(:string))
+    #end
+ 
   end
 end
