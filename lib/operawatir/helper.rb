c92f32c6b70ec786a7b0e8fe6c098ff35e7f3130
Andreas Tolf Tolfsen <andreastt@opera.com>
2010-09-24 16:08:43 +0200

    Adding SpartanFormatter, OperaHelperFormatter not overwriting -f argument.

--- lib/operawatir/helper.rb
+++ lib/operawatir/helper.rb
@@ -4,6 +4,7 @@ begin
   require "spec/runner/formatter/base_text_formatter"
   require "rbconfig"
   require "pathname"
+  require File.expand_path(File.dirname(__FILE__) + "../../../utils/formatters/operahelper_formatter.rb")
 rescue LoadError
 end
 
@@ -257,60 +258,16 @@ module OperaWatir
   end
 end
 
-class OperaHelperFormatter < Spec::Runner::Formatter::BaseTextFormatter
-  def example_failed (example, counter, failure)
-    output.puts message example, colorize_failure("FAILED", failure)
-    output.flush
-  end
-
-  def example_passed (example)
-    output.puts message example, green("PASSED")
-    output.flush
-  end
-
-  def example_pending (example, message)
-    output.puts message example, blue("PENDING")
-    output.flush
-  end
-
-  def example_group_started (example_group_proxy)
-    message = line +
-      example_group_proxy.description + " (" + example_group_proxy.examples.size.to_s + " examples)\n" +
-      line
-
-    output.puts(message)
-    output.flush
-  end
-
-  def message (example, text)
-    example.description.ljust(OperaWatir::Helper.terminal_size[0] - 7) + text
-  end
-
-  def line
-    message = ""
-    i = 0
-
-    begin
-      message += "-"
-      i += 1
-    end while i < OperaWatir::Helper.terminal_size[0]
-
-    message += "\n"
-    return message
-  end
-end
-
 module Spec
   module Runner
     class Options
       def formatters
-        @format_options = [["OperaHelperFormatter", @output_stream]]
+        @format_options = [["OperaHelperFormatter", @output_stream]] if @format_options.nil?
         @formatters ||= load_formatters(@format_options, EXAMPLE_FORMATTERS)
       end
     end
   end
 end
-
 
 OperaWatir::Helper.execute
 
