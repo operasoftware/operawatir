519aa46f59fcf0a3ff13e0ae9126f8f78e43a7db
Adam Minchinton <adamm@opera.com>
2010-10-07 12:31:20 +0200

    Added keypress and load page function

--- lib/operawatir/quickwidgets/quick_addressfield.rb
+++ lib/operawatir/quickwidgets/quick_addressfield.rb
@@ -6,6 +6,20 @@ module OperaWatir
     def correct_type?
       @element.getType == WIDGET_ENUM_MAP[:addressfield]
     end
+
+    ######################################################################
+    # Presses the key, and waits for loading to finish
+    #
+    # @param [String]  key         key to press (e.g. "a" or "backspace")
+    # @param [String]  modifiers   optional modifier(s) to hold down while pressing the key (e.g. :shift, :ctrl, :alt, :meta)
+    #
+    # @return [int] Window ID of the window shown or 0 if no window is shown
+    #
+    def load_page_with_key_press(key, *modifiers)
+      wait_start
+      click()
+      wait_for_window_loaded("")
+    end
     
   end
 end
