383e2d6581823679dda52c1a3af36291cb9a15f3
Adam Minchinton <adamm@opera.com>
2010-10-14 15:17:18 +0200

    Added double click

--- lib/operawatir/quickwidgets/quick_treeitem.rb
+++ lib/operawatir/quickwidgets/quick_treeitem.rb
@@ -24,6 +24,22 @@ module OperaWatir
       sleep(0.2);
     end
     
+    ######################################################################
+    # Double clicks the tree item, and waits for the window with 
+    # window name win_name to be shown
+    #
+    # @param [String] win_name name of the window that will be opened (Pass a blank string for any window)
+    #
+    # @return [int] Window ID of the window shown or 0 if no window is shown
+    #
+    def open_window_with_double_click(win_name)
+      wait_start
+      click(:left, 2)
+      wait_for_window_shown(win_name)
+    end
+    
+    alias_method :open_dialog_with_double_click, :open_window_with_double_click
+
   end
 end
 
