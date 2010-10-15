24f301d8cdee064e2a54283597e814835792678b
Karianne Ekern <karie@opera.com>
2010-10-15 10:28:21 +0200

    alias open_page_with_click, also open_tab_with_click

--- lib/operawatir/quickwidgets/quick_tab.rb
+++ lib/operawatir/quickwidgets/quick_tab.rb
@@ -5,17 +5,17 @@ module OperaWatir
     # def correct_type? - super works fine here
     
     ######################################################################
-    # Clicks the button, and waits for the window with window name 
-    # win_name to be shown / switches to the page
+    # Clicks the tab button, and waits for the tab to be shown / switches to the page
     #
     # @param [String] win_name name of the window that will be opened (Pass a blank string for any window)
     #
     # @return [int] Window ID of the window shown or 0 if no window is shown
     #
-    def open_page_with_click()
-      open_window_with_click("")
+    def open_tab_with_click()
+      open_window_with_click("Document Window")
     end
-    
+    alias_method :open_page_with_click, :open_tab_with_click
+        
     ######################################################################
     # Gets a string representation of the tab button 
     #
