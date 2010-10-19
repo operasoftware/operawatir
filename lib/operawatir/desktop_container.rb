58fb605ee666b5c47209246291ac849600c9095a
Karianne Ekern <karie@opera.com>
2010-10-19 10:26:20 +0200

    Updated tab documentation

--- lib/operawatir/desktop_container.rb
+++ lib/operawatir/desktop_container.rb
@@ -24,7 +24,7 @@ module OperaWatir
     #
     # @param [String] how   Method to find the element. 
     # @param [String] what  Search text to find the element with. Currently title of the tab
-    #     or [Fixnum] what  Position of tab  
+    #     or [Fixnum] what  Position of tab. Note first tab has position 0
     #
     # @example
     #   $browser.quick_toolbar(:name, "Pagebar").quick_tab(:pos, 1)
