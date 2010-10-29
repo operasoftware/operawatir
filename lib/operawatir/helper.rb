8312af456a36283c5b8ed81659052671dc17e943
Andreas Tolf Tolfsen <andreastt@opera.com>
2010-10-29 19:10:50 +0200

    *** empty log message ***

--- lib/operawatir/helper.rb
+++ lib/operawatir/helper.rb
@@ -1,16 +1,4 @@
-require 'operawatir'
+crequire 'operawatir'
 require 'operawatir/waiter'
 
 OperaWatir::Waiter.run!
-
-# begin
-#   require "rubygems"
-#   require "operawatir"
-#   require "spec/runner/formatter/base_text_formatter"
-#   require "rbconfig"
-#   require "pathname"
-#   require File.expand_path(File.dirname(__FILE__) + "../../../utils/formatters/operahelper_formatter.rb")
-# rescue LoadError
-# end
-# 
-# OperaWatir::Helper.execute
