1e23586ff1974291524f7a6afc3c84711e44ff48
Andreas Tolf Tolfsen <andreastt@opera.com>
2010-11-08 14:46:34 +0100

    Oops, added another layer.

--- lib/operawatir/version.rb
+++ lib/operawatir/version.rb
@@ -1,14 +1,12 @@
 module OperaWatir
-  class OperaWatir
 
-    # @private
-    VERSION = '0.2.15'.freeze
+  # @private
+  VERSION = '0.2.15'.freeze
 
-    # @return [String] current version of OperaWatir
-    def self.version
-      VERSION
-    end
-
+  # @return [String] current version of OperaWatir
+  def self.version
+    VERSION
   end
+
 end
 
