29642d8c6761b13031471df23b4ade3660ba9cdd
Karianne Ekern <karie@opera.com>
2010-11-26 17:38:40 +0100

    Commented api change

--- lib/operawatir/quickwidgets/quick_tab.rb
+++ lib/operawatir/quickwidgets/quick_tab.rb
@@ -29,6 +29,8 @@ module OperaWatir
     #
     # @raise [DesktopExceptions::UnknownObjectException] if the target is not a tab
     #
+    # @return [int] the number of tabs in this tab group, or 1 if this is not a tab group button,
+    #               in which case it represents only 1 tab, itself
     def group_with_drag(tab_target)
       raise(Exceptions::UnknownObjectException) unless tab_target.type == :tabbutton
       
