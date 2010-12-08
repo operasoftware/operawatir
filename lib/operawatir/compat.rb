af94401c035b72358eb6cae3f10b3cbd8821992f
Chris Lloyd <christopherl@opera.com>
2010-12-08 17:23:22 +0100

    Rewrote selector engine. Things need to settle down a little, but it's looking sweet so far. 454/1035 tests failing.

--- lib/operawatir/compat.rb
+++ lib/operawatir/compat.rb
@@ -1,12 +1,13 @@
 require 'operawatir/compat/deprecation'
 require 'operawatir/compat/browser'
-require 'operawatir/compat/window'
+require 'operawatir/compat/element_finders'
 
 module OperaWatir
   
   def self.compatibility!
     Browser.send :include, Compat::Browser
-    Window.send :include, Compat::Window
+    Window.send :include, Compat::ElementFinders
+    Collection.send :include, Compat::ElementFinders
     
     # TODO Ruby Modules can't override methods defined in their included klass
     # Requiring the files is OK, but there needs to be some way of detecting
@@ -14,7 +15,6 @@ module OperaWatir
     
     require 'operawatir/compat/collection'
     require 'operawatir/compat/element'
-    require 'operawatir/compat/selector'
   end
   
 end
--- lib/operawatir/selector.rb
+++ lib/operawatir/selector.rb
@@ -1,42 +1,74 @@
 class OperaWatir::Selector
-  BASIC_TYPES = [:id, :class, :tag, :css, :xpath]
+  BASIC_TYPES = [:id, :class_name, :tag_name, :css, :xpath, :index, :attribute]
 
-  attr_accessor :type, :attribute, :value
+  attr_accessor :collection, :sexp
 
-  def initialize(collection, type, value)
-    self.collection, self.type, self.value = collection, type, value
-  end
-
-  def apply_to(elements)
-    if basic?
-      collection.parent.send(finder_method, value)
-    else
-      send(finder_method, elements || collection.parent.raw_elements)
-    end.tap do |result|
-      raise(OperaWatir::Exceptions::UnknownObjectException) if result.empty?
-    end
-  end
-
-private
-
-  attr_accessor :collection
-
-  def basic?
-    BASIC_TYPES.include?(type)
-  end
-
-  def self.operator(obj)
-    obj.respond_to?(:match) ? :match : :==
-  end
-
-  def finder_method
-    "find_elements_by_#{type}".to_sym
+  def initialize(collection, sexp=nil)
+    self.collection, self.sexp = collection, sexp
   end
   
-  def find_elements_by_attribute(elements)
-    elements.select do |element|
-      value.all? do |key, val|
-        element.send(key).send(self.class.operator(val), val)
+  # def apply_to(elements)
+  #   if basic?
+  #     collection.parent.send(finder_method, value)
+  #   else
+  #     send(finder_method, elements || collection.parent.raw_elements)
+  #   end
+  # end
+  
+  def elements
+    eval(parent)
+  end
+  
+  def eval(exp=sexp)
+    exp.is_a?(Array) ? apply(*exp.map {|x| eval(x)}) : exp
+  end
+  
+  def apply(fn, *args)
+    elms = args.shift
+    
+    # Say hello to the the ol' send trick.
+    # Private methods can be called when using send. This "bug" was briefly 
+    # disabled in 1.9 but people complained because it's very handy (but bad).
+    if elms.nil?
+      collection.parent.send("find_elements_by_#{fn}", *args)
+    else
+      collection.send(:_elms=, elms)
+      collection.send("find_elements_by_#{fn}", *args)
+    end
+  end
+  
+  BASIC_TYPES.each do |name|
+    define_method name do |val|
+      self.sexp = [name, sexp, val]
+      self
+    end
+  end
+  
+  alias_method :tag, :tag_name
+  
+  [:join, :antijoin].each do |name|
+    define_method name do |&blk|
+      list = BlockBuilder.new(self)
+      blk.call(list)
+      self.sexp = list.items.inject([name]) do |items, item|
+        items << item.sexp
+      end
+      self
+    end
+  end
+  
+private
+  
+  class BlockBuilder
+    attr_accessor :selector, :items
+    
+    def initialize(selector)
+      self.selector, self.items = selector, []
+    end
+    
+    def method_missing(*args, &blk)
+      OperaWatir::Selector.new(selector.collection, selector.sexp).send(*args, &blk).tap do |s|
+        items << s
       end
     end
   end
