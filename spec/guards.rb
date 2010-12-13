467829b043a896990e910b962540bc040547c8a8
Chris Lloyd <christopherl@opera.com>
2010-12-13 16:12:09 +0100

    Legacy Watirspec tests now running with Bug guards.

--- spec/guards.rb
+++ spec/guards.rb
@@ -15,22 +15,22 @@ module WatirSpec
     module Helpers
       def deviates_on(*browsers)
         guard = WatirSpec::Guard.new :deviation, browsers, :file => caller.first
-        yield unless guard.guarded?(OperaWatir::Waiter.browser)
+        yield unless guard.guarded?(OperaWatir::Helper.browser)
       end
 
       def not_compliant_on(*browsers)
         guard = WatirSpec::Guard.new :non_compliance, browsers, :file => caller.first
-        yield unless guard.guarded?(OperaWatir::Waiter.browser)
+        yield unless guard.guarded?(OperaWatir::Helper.browser)
       end
 
       def compliant_on(*browsers)
         guard = WatirSpec::Guard.new :compliance, browsers, :file => caller.first
-        yield unless guard.guarded?(OperaWatir::Waiter.browser)
+        yield unless guard.guarded?(OperaWatir::Helper.browser)
       end
 
       def bug(url, *browsers)
         guard = WatirSpec::Guard.new :bug, browsers, :file => caller.first, :url => url
-        yield unless guard.guarded?(OperaWatir::Waiter.browser)
+        yield unless guard.guarded?(OperaWatir::Helper.browser)
       end
     end
     
