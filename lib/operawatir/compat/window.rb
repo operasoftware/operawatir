9a49deaf79efe5a0e9febaac856330499544f775
Chris Lloyd <christopherl@opera.com>
2010-12-14 15:11:41 +0100

    Fix.

--- lib/operawatir/compat/window.rb
+++ lib/operawatir/compat/window.rb
@@ -1,13 +1,12 @@
 module OperaWatir
   module Compat
-    module Browser
+    module Window
 
       def contains_text(text)
         text.index(text)
       end
       
       alias_method :element_by_xpath, :find_by_xpath
-      
       
     end
   end
