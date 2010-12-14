1b47a51600f023a4cf6f1725d78d24073edd5c6d
Andreas Tolf Tolfsen <andreastt@opera.com>
2010-12-14 12:34:18 +0100

    Defining Compat module in compat.rb instead of compat/browser.rb

--- lib/operawatir/compat/browser.rb
+++ lib/operawatir/compat/browser.rb
@@ -1,11 +1,7 @@
-module OperaWatir
-  module Compat
-    module Browser
+module OperaWatir::Compat::Browser
 
-      # def method_missing(method, *args, &blk)
-      #   active_window.send(method, *args, &blk)
-      # end
+  # def method_missing(method, *args, &blk)
+  #   active_window.send(method, *args, &blk)
+  # end
 
-    end
-  end
 end
