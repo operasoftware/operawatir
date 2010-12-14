5bcdd3c43fb2e66d515b0ad5e84d6dbc4340c071
Andreas Tolf Tolfsen <andreastt@opera.com>
2010-12-14 17:37:18 +0100

    Committed an unresolved file previous, fixing

--- lib/operawatir/compat/window.rb
+++ lib/operawatir/compat/window.rb
@@ -5,34 +5,15 @@ module OperaWatir
       def contains_text(str)
         text.index(str)
       end
-<<<<<<< HEAD
       
       #alias_method :element_by_xpath, :find_by_xpath
-#      alias_method :elements_by_xpath, :find_elements_by_xpath
 
       def elements_by_xpath(value)
-#        OperaWatir::Collection.new(self).tap do |c|
-#          c.selector.send(:xpath, value)
-#        end._elms
-
-        #self.parent.send('find_element_by_xpath', value)
-
-        #self.find_element_by_xpath(value)
         find_by_xpath(value)
-
       end
 
       alias_method :element_by_xpath, :elements_by_xpath
 
-#      def element_by_xpath(value)
-#        OperaWatir::Collection.new(self).tap do |c|
-#          c.selector.send(:xpath, value)
-#        end
-#      end
-      
-=======
-
->>>>>>> 4b4eba4db3b4ea80ab071869b5d272dfc6637a5b
     end
   end
 end
