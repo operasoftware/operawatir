48cd26fb6e5af64e0a7193070d67003a5b2f1181
Stuart Knightley <stuartk@opera.com>
2010-12-14 16:56:23 +0100

    Fixed regression in earlier merge

--- lib/operawatir/compat/element_finders.rb
+++ lib/operawatir/compat/element_finders.rb
@@ -1,8 +1,9 @@
 module OperaWatir::Compat::ElementFinders
-  
-  def self.def_element(tag, plural, &blk)
+
+  def self.def_element(tag, plural, opts={}, &blk)
     blk = block_given? ? blk : lambda {|selector| selector.tag tag}
 
+    default_method = opts[:how] || :id
 
     define_method tag do |*arguments|
       OperaWatir::Collection.new(self).tap do |c|
@@ -17,11 +18,11 @@ module OperaWatir::Compat::ElementFinders
       end
     end
   end
-  
-  
-  
+
+
+
   def_element :area, :areas
-  
+
   def_element :button, :buttons, :how => :value do |selector|
     selector.join do |elm|
       elm.tag(:button)
@@ -33,112 +34,112 @@ module OperaWatir::Compat::ElementFinders
       end
     end
   end
-    
+
   def_element :checkbox, :checkboxes, :how => :name do |selector|
     selector.tag(:input).attribute :type => 'checkbox'
   end
-  
+
   def_element :dd, :dds
-  
+
   def_element :del, :dels
-  
+
   def_element :div, :divs
-  
+
   def_element :dl, :dls
-  
+
   def_element :dt, :dts
-  
+
   def_element :dt, :dts
-  
+
   def_element :em, :ems
-  
+
   def_element :file_field, :file_fields, :how => :name do |selector|
     selector.tag(:input).attribute :type => 'file'
   end
-  
+
   def_element :hidden, :hiddens, :how => :name do |selector|
     selector.tag(:input).attribute :type => 'hidden'
   end
-  
+
   def_element :h1, :h1s
-  
+
   def_element :h2, :h2s
-  
+
   def_element :h3, :h3s
-  
+
   def_element :h4, :h4s
-  
+
   def_element :h5, :h5s
-  
+
   def_element :h6, :h6s
-  
+
   def_element :image, :images do |selector|
     selector.tag :img
   end
-  
+
   def_element :ins, :inses
-  
+
   def_element :label, :labels, :how => :text
-  
+
   def_element :li, :lis
-  
+
   # Oh, nevermind the ACTUAL LINK ELEMENT
   def_element :link, :links, :how => :href do |selector|
     selector.tag :a
   end
-  
+
   def_element :map, :maps
-  
+
   def_element :meta, :metas
-  
+
   def_element :ol, :ols
-  
+
   def_element :option, :options, :how => :text
-  
+
   def_element :p, :ps
-  
+
   def_element :pre, :pres
-  
+
   def_element :radio, :radios
-  
+
   def_element :select_list, :select_lists do |selector|
     selector.tag :select
   end
-  
+
   def_element :span, :spans
-  
+
   def_element :strong, :strongs
-  
+
   # Oh, nevermind the ACTUAL BODY ELEMENT
   def_element :body, :bodies do |selector|
     selector.tag :tbody
   end
-  
+
   def_element :cell, :cells do |selector|
     selector.tag :td
   end
-  
+
   def_element :table_footer, :table_footers do |selector|
     selector.tag :tfoot
   end
-  
+
   def_element :table_header, :table_headers do |selector|
     selector.tag :thead
   end
-  
+
   def_element :table_row, :table_rows do |selector|
     selector.tag :tr
   end
-  
+
   def_element :table, :tables
-  
+
   def_element :text_field, :text_fields do |selector|
     selector.join do |elm|
       elm.tag(:input).attribute :type => 'text'
       elm.tag :textarea
     end
   end
-  
+
   def_element :ul, :uls
-  
+
 end
--- lib/operawatir/compat/window.rb
+++ lib/operawatir/compat/window.rb
@@ -5,9 +5,7 @@ module OperaWatir
       def contains_text(text)
         text.index(text)
       end
-      
-      alias_method :element_by_xpath, :find_by_xpath
-      
+
     end
   end
 end
