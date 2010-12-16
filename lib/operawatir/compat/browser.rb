d8b85c0a97f8983df2094789979c19475f6b93eb
Stuart Knightley <stuartk@opera.com>
2010-12-16 11:16:34 +0100

    Add all active_window methods to Watir 1 browser

--- lib/operawatir/compat/browser.rb
+++ lib/operawatir/compat/browser.rb
@@ -1,6 +1,15 @@
 module OperaWatir
   module Compat
     module Browser
+
+      def method_missing(method, *args, &blk)
+        if active_window.respond_to? method
+          active_window.send(method, *args, &blk)
+        else
+          super
+        end
+      end
+
     end
   end
 end
