c5bda7e3b919fabc0ece6c687087f552825c217d
Andreas Tolf Tolfsen <andreastt@opera.com>
2010-12-16 20:52:05 +0100

    Formatting documentation and general cleanup

--- lib/operawatir/exceptions.rb
+++ lib/operawatir/exceptions.rb
@@ -15,18 +15,17 @@ module OperaWatir::Exceptions
 
   # Raised when specifying an unknown way of finding an element.
   #
-  # @example
-  #   browser.divs(:weird_method, 10)
+  # @example browser.divs(:weird_method, 10)
   class MissingWayOfFindingObjectException < OperaWatirException; end
   
   # Raised when trying to switch to an unknown frame.
   class UnknownFrameException < OperaWatirException; end
 
-  # Raised when performing an action which has not yet been implemented
-  # by OperaWatir.
+  # Raised when performing an action which has not yet been
+  # implemented by OperaWatir.
   class NotImplementedException < OperaWatirException; end
 
-  # @todo Document this
+  # TODO Document this
   class NoValueFoundException < OperaWatirException; end
 
   # Raised when trying to perform an action on an element which is
--- lib/operawatir/quickwidgets/quick_addressfield.rb
+++ lib/operawatir/quickwidgets/quick_addressfield.rb
@@ -1,21 +1,19 @@
 module OperaWatir
   class QuickAddressField < QuickEditField
 
-    # @private
     # Checks the type of the widget is correct
+    #
+    # @private
     def correct_type?
       @element.getType == WIDGET_ENUM_MAP[:addressfield]
     end
 
-    ######################################################################
     # Enters the url into the address field, and waits for page loading 
     # to finish
     #
-    # @param [String] url   Web Address to load
-    #
+    # @param  [String] URL to load
     # @return [String] text in the address field after the page is loaded
-    #                   or a blank string
-    #
+    #                  or a blank string
     def load_page_with_url(url)
       # Enters text in a field and then hits enter
       enter_text_and_hit_enter(url)
