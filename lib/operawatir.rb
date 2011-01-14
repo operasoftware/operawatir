19f8564e65bd525eecc7a1f82c43e63130fe4ebb
Andreas Tolf Tolfsen <andreastt@opera.com>
2011-01-14 14:15:52 +0100

    Using a better method names for activating and querying which API to use

--- lib/operawatir.rb
+++ lib/operawatir.rb
@@ -19,11 +19,22 @@ $KCODE = 'u'  # UTF-8 support
 # TODO This should be replaced when we decide upon a better way of
 # enabling the Watir 2 API.
 module OperaWatir
-  @use_version = 1
+  @current_api = 1
 
-  # Activates the Watir 2 API.
-  def self.use_version(number=@use_version)
-    @use_version = number
+  # Set the API version you wish to use.  Please note that setting
+  # this after OperaWatir::Browser has been initialized will have no
+  # affect.
+  #
+  # @param number [Integer] API version to use
+  def self.api=(number)
+    @current_api = number.to_i
+  end
+
+  # Queries which API to use.
+  #
+  # @return [Integer] Desired API version
+  def self.api
+    @current_api
   end
 end
 
