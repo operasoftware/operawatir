9c0662448e873332d9505ade13ee0e342267d931
Andreas Tolf Tolfsen <andreastt@opera.com>
2011-01-20 17:21:30 +0100

    SpartanFormatter is now using the format required by RSpec 2.0.1

--- utils/formatters/spartan_formatter.rb
+++ utils/formatters/spartan_formatter.rb
@@ -1,29 +1,29 @@
 # SpartanFormatter is intended for use with Opera's internal testing
 # environment SPARTAN.
 
-require "spec/runner/formatter/base_text_formatter"
+require 'rspec/core/formatters/base_text_formatter'
 
-class SpartanFormatter < Spec::Runner::Formatter::BaseTextFormatter
-  def example_failed(example, counter, failure)
+class SpartanFormatter < RSpec::Core::Formatters::BaseTextFormatter
+  def example_failed(example)
     message = "#{example.description}\tFAILED\n"
-    output.puts(message)
-    output.flush
+    output.print message
   end
 
   def example_passed(example)
     message = "#{example.description}\tPASSED\n"
-    output.puts(message)
-    output.flush
+    output.print message
   end
 
-  def example_pending(example, message, deprecated_pending_location=nil)
+  def example_pending(example)
     message = "#{example.description}\tPENDING\n"
-    output.puts(message)
-    output.flush
+    output.print message
   end
 
-  def dump_summary(duration, example_count, failure_count, pending_count); end
-  def dump_pending; end
-  def dump_failure(counter, failure); end
-end
+  def start_dump
+    output.puts
+  end
 
+#  def dump_summary(duration, example_count, failure_count, pending_count); end
+#  def dump_pending; end
+#  def dump_failure(counter, failure); end
+end
