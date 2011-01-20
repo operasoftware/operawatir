4dd8daf45e2a2252e334e3a7a82320b7612d7872
Andreas Tolf Tolfsen <andreastt@opera.com>
2011-01-20 11:56:12 +0100

    Now loading new selenium library instead, also removing winp

--- lib/operawatir.rb
+++ lib/operawatir.rb
@@ -4,9 +4,8 @@ require "operawatir/version"
 require "java"
 include Java
 
-%w(commons-jxpath-1.3.jar protobuf-java-2.3.0.jar webdriver-common.jar
-   winp-1.14.jar webdriver-opera.jar).
-   each { |jar| require "operadriver/#{jar}" }
+%w(commons-jxpath-1.3.jar protobuf-java-2.3.0.jar selenium-common.jar
+   webdriver-opera.jar).each { |jar| require "operadriver/#{jar}" }
 
 include_class org.openqa.selenium.WebDriver
 include_class org.openqa.selenium.RenderedWebElement
