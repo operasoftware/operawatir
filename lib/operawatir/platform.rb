30e361292b1be4d6dbb53c6cdbd9319014e22c41
Andreas Tolf Tolfsen <andreastt@opera.com>
2011-02-07 15:16:38 +0100

    Removing now outdated code in OperaWatir::Platform, this is figured out by OperaDriver now

--- lib/operawatir/platform.rb
+++ lib/operawatir/platform.rb
@@ -1,36 +1,6 @@
 require 'rbconfig'
 
 class OperaWatir::Platform
-  def self.launcher
-    return ENV['OPERA_LAUNCHER'] if ENV['OPERA_LAUNCHER']
-
-    path = case os
-    when :windows
-	  # 32 bit launcher works on both 32 and 64 bit versions of Windows
-      "utils/launchers/launcher-win32-i86pc.exe"
-    when :linux
-      postfix = bitsize == 64 ? 'x86_64' : 'i686'
-      "utils/launchers/launcher-linux-#{postfix}"
-    when :macosx
-      'utils/launchers/launcher-mac'
-    end
-
-    if path
-      File.join(root, path)
-    else
-      raise Exceptions::NotImplementedException, "no known launcher for #{os.inspect}, set OPERA_LAUNCHER to override"
-    end
-
-  end
-
-  def self.opera
-    ENV['OPERA_PATH']
-  end
-
-  def self.args
-    ENV['OPERA_ARGS'] || ''
-  end
-
   def self.os
     @os ||= (
       host_os = RbConfig::CONFIG['host_os']
@@ -47,13 +17,5 @@ class OperaWatir::Platform
         raise Exceptions::NotImplementedException, "#{host_os.inspect} not supported"
       end
     )
-  end
-
-  def self.bitsize
-    ENV_JAVA['sun.arch.data.model'].to_i
-  end
-
-  def self.root
-    @root ||= File.expand_path('../../..', __FILE__)
   end
 end
