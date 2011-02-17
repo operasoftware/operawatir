f3ec6490ab23a9c7e5746a43c2b54c1600912c48
Karianne Ekern <karie@opera.com>
2011-02-17 16:22:30 +0100

    button and adrfield test file added

---
+++ spec/operawatir/desktop/quickaddressfield_spec.rb
@@ -0,0 +1,14 @@
+require File.expand_path('../../watirspec_helper', __FILE__)
+
+describe "quick_addressfield" do
+   describe "#load_page_with_url" do
+   end
+   
+   describe "#visible_text" do
+     it "returns the visible text"
+   end
+   
+   describe "#highlighted_text" do
+     it "returns the highlighted text"
+   end
+end
---
+++ spec/operawatir/desktop/quickbutton_spec.rb
@@ -0,0 +1,34 @@
+require File.expand_path('../../watirspec_helper', __FILE__)
+
+describe "quick_widget" do
+    describe "#default?" do
+    end
+
+    describe "#open_window_with_click" do
+    end
+    
+    describe "#change_page_with_click" do
+    end
+  
+    describe "#close_window_with_click" do
+    
+    end
+  
+    describe "#load_page_with_click" do
+    end
+    
+    describe "#toggle_with_click" do
+    end
+
+    describe "#close_toolbar_with_click" do
+    end
+
+    describe "#expand_with_click" do
+    end
+
+    describe "#value" do
+    end
+  
+    describe "#wait_for_enabled" do
+    end
+end
