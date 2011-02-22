9f42cf3696fb2bd73a61b903746eac5741b0c88c
Wilhelm Joys Andersen <wilhelmja@opera.com>
2011-02-22 16:40:55 +0100

    Added tests for Window#visual_hash

--- spec/operawatir/core/window_spec.rb
+++ spec/operawatir/core/window_spec.rb
@@ -14,4 +14,25 @@ describe 'Window' do
     end
   end
 
+  describe '#visual_hash' do
+    before :each do
+      browser.url = fixture('boxes.html')
+      @reference = window.visual_hash
+    end
+
+     it 'returns a hash' do
+      @reference.length.should == 34
+    end
+
+    it 'returns identical hashes for visually identical pages' do
+      browser.url = fixture('boxes.html')
+      window.visual_hash.should == @reference
+    end
+
+    it 'returns different hashes for visually different elements' do
+      browser.url = fixture('grid.html')
+      window.visual_hash.should_not == @reference
+    end
+  end
+
 end
