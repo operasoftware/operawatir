574a7add012709c65ef0c7ab83d9498f12f355f2
Karianne Ekern <karie@opera.com>
2011-03-03 10:35:22 +0100

    Simplified window tests a bit

--- spec/operawatir/desktop/quickwindow_spec.rb
+++ spec/operawatir/desktop/quickwindow_spec.rb
@@ -5,6 +5,8 @@ describe 'QuickWindow' do
   let(:documentwindow) { browser.quick_window(:name, "Document Window") }
   let(:browserwindow) { browser.quick_window(:name, "Browser Window") }
   let(:nonexisting_window) { browser.quick_window(:name, "Doc Window") }
+    
+  subject { documentwindow }
   
   describe "#exist?" do
     it "returns true for existing window" do
@@ -17,10 +19,8 @@ describe 'QuickWindow' do
   end
 
   describe "#type" do
-    it "returns Normal for Document Window" do
-      documentwindow.type == "Normal"
-    end
-    
+    its(:type) { should == :normal }
+      
     it "returns Dialog for Dialog" do
       browser.open_dialog_with_key_press("New Preferences Dialog", "F12", :ctrl).should > 0
       browser.quick_window(:name, "New Preferences Dialog").type == "Dialog"
@@ -33,24 +33,18 @@ describe 'QuickWindow' do
   end
 
   describe "#name" do
-    it "returns window name" do
-      documentwindow.name.should_not be_empty
-    end
-    it "returns a string" do
-      documentwindow.name.should be_kind_of String
-    end
+    its(:name) { should_not be_empty }
+    its(:name) { should be_kind_of String } 
+    
     it "throws exception if unknown window" do
       expect { nonexisting_window.name }.to raise_error OperaWatir::Exceptions::UnknownObjectException
     end
   end
     
   describe "#title" do
-    it "returns title" do
-      documentwindow.title.should_not be_empty
-    end
-    it "returns a string" do
-      documentwindow.title.should be_kind_of String
-    end
+    its(:title) { should_not be_empty }
+    its(:title) { should be_kind_of String }  
+    
     it "throws exception if unknown window" do
       expect { nonexisting_window.title }.to raise_error OperaWatir::Exceptions::UnknownObjectException
     end
@@ -77,24 +71,18 @@ describe 'QuickWindow' do
   end
     
   describe "#window_id" do
-    it "is non-zero integer" do
-      documentwindow.window_id.should_not be_zero
-    end
-    it "returns an integer" do
-      documentwindow.window_id.should be_integer
-    end
+    its(:window_id) { should be_integer }
+    its(:window_id) { should_not be_zero }
+    
     it "throws exception if unknown window" do
       expect { nonexisting_window.window_id }.to raise_error OperaWatir::Exceptions::UnknownObjectException
     end
   end
     
   describe "#window_info_string" do
-    it "returns string" do
-        documentwindow.window_info_string.should be_kind_of String
-    end
-    it "is nonempty" do
-        documentwindow.window_info_string.should_not be_empty
-    end
+    its(:window_info_string) { should be_kind_of String }
+    its(:window_info_string) { should_not be_empty }
+    
     it "throws exception if unknown window" do
       expect { nonexisting_window.window_info_string }.to raise_error OperaWatir::Exceptions::UnknownObjectException
     end
