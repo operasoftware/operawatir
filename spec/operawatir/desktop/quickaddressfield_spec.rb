27d5def16dccf89bf92e32430e0796a5ccbbc1fc
Karianne Ekern <karie@opera.com>
2011-03-03 10:18:02 +0100

    Refactored tests a bit

--- spec/operawatir/desktop/quickaddressfield_spec.rb
+++ spec/operawatir/desktop/quickaddressfield_spec.rb
@@ -1,34 +1,38 @@
 require File.expand_path('../../watirspec_helper', __FILE__)
 
-describe "quick_addressfield" do
+describe "QuickAddressfield" do
+  
+   let(:addressfield) { browser.quick_toolbar(:name, "Document Toolbar").quick_addressfield(:name, "tba_address_field") }
+   let(:unknown_addressfield) { browser.quick_toolbar(:name, "Document Toolbar").quick_addressfield(:name, "no_address_field") }
+  
    describe "#load_page_with_url" do
      it "loads page" do
-      browser.quick_toolbar(:name, "Document Toolbar").quick_addressfield(:name, "tba_address_field").load_page_with_url("opera:debug").should == "opera:debug"
+       addressfield.load_page_with_url("opera:debug").should == "opera:debug"
      end
      it "throws exception if unknown addressfield" do
-       expect { browser.quick_toolbar(:name, "Document Toolbar").quick_addressfield(:name, "tba_address_fieldd").highlighted_text }.to raise_error OperaWatir::Exceptions::UnknownObjectException
+       expect { unknown_addressfield.highlighted_text }.to raise_error OperaWatir::Exceptions::UnknownObjectException
      end
    end
 
    describe "text" do
      before(:each) do
-       browser.quick_toolbar(:name, "Document Toolbar").quick_addressfield(:name, "tba_address_field").load_page_with_url("opera:debug").should == "opera:debug"
+       addressfield.load_page_with_url("opera:debug").should == "opera:debug"
     end
-   describe "#visible_text" do
+    describe "#visible_text" do
      it "returns the visible text" do
-       browser.quick_toolbar(:name, "Document Toolbar").quick_addressfield(:name, "tba_address_field").visible_text.should == "debug"
+       addressfield.visible_text.should == "debug"
      end
      it "throws exception if unknown addressfield" do
-       expect { browser.quick_toolbar(:name, "Document Toolbar").quick_addressfield(:name, "tba_address_fieldd").highlighted_text }.to raise_error OperaWatir::Exceptions::UnknownObjectException
+       expect { unknown_addressfield.highlighted_text }.to raise_error OperaWatir::Exceptions::UnknownObjectException
      end
    end
    
    describe "#highlighted_text" do
      it "returns the highlighted text" do
-       browser.quick_toolbar(:name, "Document Toolbar").quick_addressfield(:name, "tba_address_field").highlighted_text.should == ""
+       addressfield.highlighted_text.should == ""
      end
      it "throws exception if unknown addressfield" do
-       expect { browser.quick_toolbar(:name, "Document Toolbar").quick_addressfield(:name, "tba_address_fieldd").highlighted_text }.to raise_error OperaWatir::Exceptions::UnknownObjectException
+       expect { unknown_addressfield.highlighted_text }.to raise_error OperaWatir::Exceptions::UnknownObjectException
      end
     end
    end
