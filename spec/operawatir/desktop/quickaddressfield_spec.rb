6a4e58c52aa7080eeca6e4ea6da5e2c923507c53
Karianne Ekern <karie@opera.com>
2011-03-03 10:43:01 +0100

    Simplified addressfield tests a bit

--- spec/operawatir/desktop/quickaddressfield_spec.rb
+++ spec/operawatir/desktop/quickaddressfield_spec.rb
@@ -5,6 +5,8 @@ describe "QuickAddressfield" do
    let(:addressfield) { browser.quick_toolbar(:name, "Document Toolbar").quick_addressfield(:name, "tba_address_field") }
    let(:unknown_addressfield) { browser.quick_toolbar(:name, "Document Toolbar").quick_addressfield(:name, "no_address_field") }
   
+   subject { addressfield }
+     
    describe "#load_page_with_url" do
      it "loads page" do
        addressfield.load_page_with_url("opera:debug").should == "opera:debug"
@@ -14,26 +16,27 @@ describe "QuickAddressfield" do
      end
    end
 
-   describe "text" do
-     before(:each) do
-       addressfield.load_page_with_url("opera:debug").should == "opera:debug"
-    end
-    describe "#visible_text" do
-     it "returns the visible text" do
-       addressfield.visible_text.should == "debug"
-     end
-     it "throws exception if unknown addressfield" do
+    describe "#text" do
+      its(:text) { should == "opera:debug" }
+        
+      it "throws exception if unknown addressfield" do
        expect { unknown_addressfield.highlighted_text }.to raise_error OperaWatir::Exceptions::UnknownObjectException
-     end
+      end
+    end
+    
+    describe "#visible_text" do
+      its(:visible_text) { should == "debug" }
+    
+      it "throws exception if unknown addressfield" do
+       expect { unknown_addressfield.highlighted_text }.to raise_error OperaWatir::Exceptions::UnknownObjectException
+      end
    end
    
    describe "#highlighted_text" do
-     it "returns the highlighted text" do
-       addressfield.highlighted_text.should == ""
-     end
+     its(:highlighted_text) { should == "" }
+     
      it "throws exception if unknown addressfield" do
        expect { unknown_addressfield.highlighted_text }.to raise_error OperaWatir::Exceptions::UnknownObjectException
      end
     end
-   end
 end
