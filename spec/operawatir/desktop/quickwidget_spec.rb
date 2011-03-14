1d21e7bab7ebc44c77a1e2dbf6af00d94186b248
Maria Popova <mariap@opera.com>
2011-03-14 15:41:30 +0100

    updated quickwidget file

--- spec/operawatir/desktop/quickwidget_spec.rb
+++ spec/operawatir/desktop/quickwidget_spec.rb
@@ -8,13 +8,39 @@ describe 'QuickWidget' do
     end
     
     describe '#type' do
+	
+		it 'type:addressfield' do
+			browser.quick_toolbar(:name, "Document Toolbar").quick_addressfield(:name, "tba_address_field").type.should == :addressfield
+		end
+		
+		it 'type:button' do
+			browser.quick_window(:name, "Document Window").quick_toolbar(:name, "Document Toolbar").quick_button(:name, "tbb_Forward").type.should == :button
+		end
+		
+		it 'type:tabbutton' do
+			browser.quick_window(:name, "Browser Window").quick_toolbar(:name, "Pagebar").quick_tab(:name, "Tab 0").type.should == :tabbutton
+		end
+		
     end
  
     describe '#verify_text' do
+	
+		it 'verify the text id' do
+			browser.open_dialog_with_action("New Preferences Dialog", "Show preferences").should > 0
+			browser.quick_button(:name, "button_OK").verify_text("DI_ID_OK").should be_true
+			browser.close_dialog("New Preferences Dialog").should > 0
+		end
     end
   
     #This is deprecated
     describe '#verify_includes_text' do
+	
+		it 'verify the include text id' do
+			browser.open_dialog_with_action("New Preferences Dialog", "Show preferences").should > 0
+			browser.quick_button(:name, "button_OK").verify_includes_text("DI_ID_OK").should be_true
+			browser.close_dialog("New Preferences Dialog").should > 0
+		end
+	
     end
     
     describe 'print_row' do
@@ -24,6 +50,14 @@ describe 'QuickWidget' do
     end
 
     describe '#focus_with_click' do
+		
+		it 'should focus the addressfield' do
+			browser.quick_toolbar(:name, "Document Toolbar").quick_addressfield(:name, "tba_address_field").focus_with_click
+			browser.quick_toolbar(:name, "Document Toolbar").quick_addressfield(:name, "tba_address_field").clear
+			browser.quick_toolbar(:name, "Document Toolbar").quick_addressfield(:name, "tba_address_field").type_text("www_address").should == "www_address"
+			browser.quick_toolbar(:name, "Document Toolbar").quick_addressfield(:name, "tba_address_field").text.should == "www_address"
+		end
+	
     end
     
     #describe '#element' do
