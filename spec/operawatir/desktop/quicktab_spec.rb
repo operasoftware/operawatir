2c3b77b0a464fd5ad6117ec081eaac120e308850
Karianne Ekern <karie@opera.com>
2011-03-15 16:12:01 +0100

    Added spec file for quicktab

---
+++ spec/operawatir/desktop/quicktab_spec.rb
@@ -0,0 +1,13 @@
+require File.expand_path('../../watirspec_desktophelper', __FILE__)
+require File.expand_path('../shared/shared', __FILE__)
+
+describe 'QuickTab' do
+  
+   let(:widget) { browser.quick_window(:name, "Browser Window").quick_toolbar(:name, "Pagebar").quick_tab(:name, "Tab 0") }
+  
+   subject { widget }
+     
+   it_behaves_like "a widget"
+   #it_behaves_like "a button"
+   
+end
