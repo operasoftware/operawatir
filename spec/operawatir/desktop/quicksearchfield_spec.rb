c47eb9c943ba74bbad6299b8946d44b71a3e5fac
Karianne Ekern <karie@opera.com>
2011-03-18 11:37:47 +0100

    Added thumbnail and searchfield test files

---
+++ spec/operawatir/desktop/quicksearchfield_spec.rb
@@ -0,0 +1,16 @@
+require File.expand_path('../../watirspec_desktophelper', __FILE__)
+require File.expand_path('../shared/shared', __FILE__)
+
+describe 'QuickSearchField' do
+  
+  let(:widget) { browser.quick_window(:name, "Document Window").quick_toolbar(:name, "Document Toolbar").quick_searchfield(:name, "tbs_MainSearch") }
+   subject { widget }
+     
+   it_behaves_like "a widget"
+   it_behaves_like "an editfield"
+   
+  describe "quick_searchfield" do
+    it "constructs searchfield by its name"
+  end
+   
+end
---
+++ spec/operawatir/desktop/quickthumbnail_spec.rb
@@ -0,0 +1,19 @@
+require File.expand_path('../../watirspec_desktophelper', __FILE__)
+require File.expand_path('../shared/shared', __FILE__)
+
+describe 'QuickThumbnail' do
+  
+   let(:widget) { browser.quick_window(:name, "Browser Window").quick_toolbar(:name, "Pagebar").quick_tab(:name, "Tab 0") }
+  
+   subject { widget }
+     
+   it_behaves_like "a widget"
+   it_behaves_like "a button"
+   
+  describe "quick_thumbnail" do
+     it "constructs thumbnail by its position"
+     it "constructs thumbnail by its name"
+     it "constructs thumbnail by its text"
+   end
+   
+end
