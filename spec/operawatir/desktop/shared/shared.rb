5d1284d1667ff274488864bcf4783374a86921ce
Karianne Ekern <karie@opera.com>
2011-03-18 09:57:39 +0100

    Added some common spectests for button, editfield and checkbox

--- spec/operawatir/desktop/shared/shared.rb
+++ spec/operawatir/desktop/shared/shared.rb
@@ -2,11 +2,20 @@ require File.expand_path('../../../watirspec_desktophelper', __FILE__)
 
 shared_examples_for 'a button' do
   describe '#default?' do
+    it "returns a boolean" do
+      [true, false].should include widget.default?
+    end
   end
   describe '#value' do
+    it "returns 0 or 1" do
+      [0, 1].should include widget.value
+    end
   end
-  #describe '#toggle_with_click' do
-  #end
+  describe '#toggle_with_click' do
+    it "returns value" do
+      [0, 1].should include widget.toggle_with_click
+    end
+  end
 end
 
 shared_examples_for 'an editfield' do
@@ -16,13 +25,44 @@ shared_examples_for 'an editfield' do
     end
   end
       
-  #describe '#type_text' do
-  #end
-  #describe '#clear' do
-  #end
+  describe '#type_text' do
+    it "types the given text" do
+      widget.type_text "some text"
+      widget.focus_with_click # *nix workaround; get rid of addressbar dropdown window
+      widget.text.should include "some text"
+    end
+  end
+  
+  describe '#clear' do
+    it "clears the editfield" do
+      widget.clear
+      widget.send :element, true #urgh
+      widget.text.should be_empty
+      
+    end
+    
+    #avoid leaving search dropdown open
+    after(:each) do  
+      widget.focus_with_click
+    end
+  end
+  
   #describe '#key_press' do
   #end
 end
+
+=begin
+shared_examples_for 'a checkbox' do
+  describe '#checked?' do
+  end
+
+  describe '#toggle_with_click' do
+  end
+
+  describe '#open_dialog_with_click' do
+  end
+end
+=end
 
 shared_examples_for 'a widget' do
   describe '#exist?' do
@@ -42,13 +82,13 @@ shared_examples_for 'a widget' do
     #its(:type) { should }
       
     describe '#enabled?' do
-      it 'should return boolean' do
+      it 'returns a boolean' do
         [true, false].should include widget.enabled?
       end
     end
        
      describe '#visible?' do
-       it 'should return boolean' do
+       it 'returns a boolean' do
         [true, false].should include widget.enabled?
        end
      end
