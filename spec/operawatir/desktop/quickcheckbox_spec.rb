cef41ca71b498af290fd982420ece8c0d86963a8
Maria <mariap@opera.com>
2011-03-21 16:17:29 +0100

    new quick checkbox file

---
+++ spec/operawatir/desktop/quickcheckbox_spec.rb
@@ -0,0 +1,35 @@
+require File.expand_path('../../watirspec_desktophelper', __FILE__)
+require File.expand_path('../shared/shared', __FILE__)
+
+describe 'QuickCheckbox' do
+
+	#describe '' do
+		before(:all) do
+			browser.open_dialog_with_action("Clear Private Data Dialog", "Delete private data").should > 0
+			browser.quick_button(:name, "Destails_expand").toggle_with_click
+		end
+   
+   		after(:all) do
+			browser.close_all_dialogs
+		end
+  
+		let(:widget) {  browser.quick_checkbox(:name, "Delete_all_cookies") }
+		subject { widget }
+    
+		it_behaves_like "a checkbox"
+   # end
+	
+    describe '#correct_type?' do
+    end
+
+    describe '#checked?' do
+    end
+    
+    describe '#toggle_with_click' do
+    end
+
+    
+    describe '#open_dialog_with_click' do
+    end
+
+end
