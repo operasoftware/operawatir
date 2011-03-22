216ce2c7173ac73ccfe1b7e8cea279da84062368
Karianne Ekern <karie@opera.com>
2011-03-22 12:08:09 +0100

    Simplified test by using same dialog for all examples, and removed describe taking two methods

--- spec/operawatir/desktop/quickcheckbox_spec.rb
+++ spec/operawatir/desktop/quickcheckbox_spec.rb
@@ -3,30 +3,26 @@ require File.expand_path('../shared/shared', __FILE__)
 
 describe 'QuickCheckbox' do
 
-	describe '#checked?, #toggle_with_click' do
-		before(:all) do
-			browser.open_dialog_with_action("Clear Private Data Dialog", "Delete private data").should > 0
-			browser.quick_button(:name, "Destails_expand").toggle_with_click
-		end
+  before(:each) do
+    browser.open_dialog_with_action("New Preferences Dialog", "Show preferences", 4)
+    browser.quick_treeview(:name, "Advanced_treeview").quick_treeitem(:string_id, "D_NEW_PREFS_SECURITY").activate_tab_with_click
+  end
    
-   	after(:all) do
-			browser.close_all_dialogs
-		end
-  
-		let(:widget) {  browser.quick_checkbox(:name, "Delete_all_cookies") }
-		subject { widget }
-    
-		it_behaves_like 'a checkbox'
+  after(:each) do
+	 	browser.close_all_dialogs
 	end
-		
-    describe '#toggle_with_click' do
-    end
+  
+  let(:widget) {  browser.quick_checkbox(:name, "Trust_info_checkbox") }
+  subject { widget }
     
-    describe '#open_dialog_with_click' do
+  it_behaves_like 'a checkbox'
+		
+  describe '#toggle_with_click' do
+  end
+    
+  describe '#open_dialog_with_click' do
 	
 		it "returns window id" do
-			browser.open_dialog_with_action("New Preferences Dialog", "Show preferences", 4).should > 0
-			browser.quick_treeview(:name, "Advanced_treeview").quick_treeitem(:string_id, "D_NEW_PREFS_SECURITY").activate_tab_with_click
 			browser.quick_button(:name, "Set_password_button").open_dialog_with_click("Change Masterpassword Dialog").should > 0
 			browser.quick_editfield(:name, "Password_edit").focus_with_click
 			browser.quick_editfield(:name, "Password_edit").type_text("masterpass1").should == "masterpass1" 
@@ -35,9 +31,8 @@ describe 'QuickCheckbox' do
 			browser.quick_button(:name, "button_OK").close_dialog_with_click("Change Masterpassword Dialog").should close_dialog
 			browser.quick_checkbox(:name, "Master_password_checkbox").open_dialog_with_click("Security Password Dialog").should > 0
 		end
-		
-    end
-
+  end
+  
 end
 
 
