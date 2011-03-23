844ddf994ce6b602b4183e03db462a183278aa58
Maria <mariap@opera.com>
2011-03-23 11:07:25 +0100

    added a method in quicktab_spec

--- spec/operawatir/desktop/quicktab_spec.rb
+++ spec/operawatir/desktop/quicktab_spec.rb
@@ -2,6 +2,9 @@ require File.expand_path('../../watirspec_desktophelper', __FILE__)
 require File.expand_path('../shared/shared', __FILE__)
 
 describe 'QuickTab' do
+
+	let(:url1) { "http://t/platforms/desktop/automated/resources/documents/page1.html" }
+	let(:url2) { "http://t/platforms/desktop/automated/resources/documents/page2.html" }
   
    let(:widget) { browser.quick_window(:name, "Browser Window").quick_toolbar(:name, "Pagebar").quick_tab(:name, "Tab 0") }
   
@@ -21,7 +24,25 @@ describe 'QuickTab' do
       it 'raises UnknownObjectException'
     end
   end
-
+  
+  describe '#activate_tab_with_click' do
+     it 'returns windowid of window activated'
+     context 'when the window is already active' do
+       it 'returns 0'
+     end
+     context 'when the tab button is not visible' do
+       it 'raises exception'
+     end
+	 it 'activates a tab with click' do
+	   browser.quick_window(:name, "Document Window").quick_toolbar(:name, "Document Toolbar").quick_addressfield(:name, "tba_address_field").load_page_with_url(url1).should == url1
+	   browser.load_window_with_action("Document Window", "Open url in new page", url2).should > 0
+	   browser.quick_window(:name, "Browser Window").quick_toolbar(:name, "Pagebar").quick_tab(:name, "Tab 0").activate_tab_with_click
+	   browser.quick_window(:name, "Browser Window").quick_toolbar(:name, "Pagebar").quick_tab(:name, "Tab 0").text.should == "Test Page 1"
+	   browser.quick_window(:name, "Browser Window").quick_toolbar(:name, "Pagebar").quick_tab(:name, "Tab 1").activate_tab_with_click
+	   browser.quick_window(:name, "Browser Window").quick_toolbar(:name, "Pagebar").quick_tab(:name, "Tab 1").text.should == "Test Page 2"
+	 end
+  end
+  
   #Note this part of api will probably change when new tab grouping is handled
   describe '#group_with_drag' do
     it 'raises UnknownObjectException if target is not a tab'
@@ -29,13 +50,4 @@ describe 'QuickTab' do
     it 'returns 1 if not a group'
   end
   
-  describe '#activate_tab_with_click' do
-    it 'returns windowid of window activated'
-    context 'when the window is already active' do
-      it 'returns 0'
-    end
-    context 'when the tab button is not visible' do
-      it 'raises exception'
-    end
-  end
 end
