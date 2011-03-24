c45af2a29f76d3ccc55036e94edfcc06a3f3d42a
Karianne Ekern <karie@opera.com>
2011-03-24 10:58:10 +0100

    Added test for widget#verifyText

--- spec/operawatir/desktop/quickwidget_spec.rb
+++ spec/operawatir/desktop/quickwidget_spec.rb
@@ -1,6 +1,7 @@
 require File.expand_path('../../watirspec_desktophelper', __FILE__)
 
 describe 'QuickWidget' do
+=begin  
     describe '#open_window_with_hover' do
       it 'opens window on hover' do
         browser.quick_window(:name, "Browser Window").quick_toolbar(:name, "Pagebar").quick_tab(:pos, 0).open_window_with_hover.should > 0
@@ -20,17 +21,33 @@ describe 'QuickWidget' do
         end
       end
     end
- 
+=end
     describe '#verify_text' do
-      it 'verifies the text id' do
-			 browser.open_dialog_with_action("New Preferences Dialog", "Show preferences").should > 0
-			 browser.quick_button(:name, "button_OK").verify_text("DI_ID_OK").should be_true
-			 browser.close_dialog("New Preferences Dialog").should > 0
+      before(:all) do
+        browser.open_dialog_with_action("New Preferences Dialog", "Show preferences", 2).should open_dialog
+        browser.quick_treeview(:name, "Web_search_treeview").should be_visible
+        @last_row = browser.quick_treeview(:name, "Web_search_treeview").num_treeitems - 1
+        browser.quick_treeview(:name, "Web_search_treeview").quick_treeitem(:pos, [@last_row,0]).focus_with_click
       end
       
-      it "verifies strings with string substitution"
+      after(:all) do
+        browser.close_dialog("New Preferences Dialog").should close_dialog
+      end
+
+      it 'verifies simple string by string_id' do
+			 browser.quick_button(:name, "button_OK").verify_text("DI_ID_OK").should be_true
+      end
+      
+      it 'verifies strings with string substitution' 
+
+      it 'verifies strings with ampersand' do
+        puts "click on item in row #{@last_row}"
+        puts browser.string("S_FIND_IN_PAGE")
+        puts browser.quick_treeview(:name, "Web_search_treeview").quick_treeitem(:pos, [@last_row,0]).text
+        browser.quick_treeview(:name, "Web_search_treeview").quick_treeitem(:pos, [@last_row,0]).verify_text("S_FIND_IN_PAGE").should be_true
+      end
     end
-  
+=begin  
     #NOTE: This is deprecated
     describe '#verify_includes_text' do
       it 'verifies the include text id' do
@@ -46,7 +63,7 @@ describe 'QuickWidget' do
 			 browser.quick_toolbar(:name, "Document Toolbar").quick_addressfield(:name, "tba_address_field").focus_with_click
       end
 	  end
-    
+=end   
 	  # Private
 	  # Also: Test in shared.rb not here
     #describe '#element' do
