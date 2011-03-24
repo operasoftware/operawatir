3ccdca90478c496377cd7c1f7da1dc26b2e11af8
Karianne Ekern <karie@opera.com>
2011-03-24 14:37:30 +0100

    Added spec test file for QuickRadioButton

---
+++ spec/operawatir/desktop/quickradiobutton_spec.rb
@@ -0,0 +1,23 @@
+require File.expand_path('../../watirspec_desktophelper', __FILE__)
+require File.expand_path('../shared/shared', __FILE__)
+
+describe 'QuickRadiobox' do
+
+  before(:each) do
+    browser.open_dialog_with_action("New Preferences Dialog", "Show preferences", 4)
+    browser.quick_treeview(:name, "Advanced_treeview").quick_treeitem(:string_id, "D_NEW_PREFS_COOKIES").activate_tab_with_click
+  end
+   
+  after(:each) do
+    browser.close_all_dialogs
+  end
+  
+  let(:widget) {  browser.quick_radiobutton(:name, "Accept_cookies_radio") }
+  subject { widget }
+    
+  it_behaves_like 'a checkbox'
+  
+  its(:type) { should == :radiobutton }
+    
+  
+end
