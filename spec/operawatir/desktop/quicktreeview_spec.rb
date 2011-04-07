f2de7873f6e87eaa34e4e1d7be26fc803a991f02
Karianne Ekern <karie@opera.com>
2011-04-07 08:33:32 +0200

    Added treeview spec

---
+++ spec/operawatir/desktop/quicktreeview_spec.rb
@@ -0,0 +1,15 @@
+require File.expand_path('../../watirspec_desktophelper', __FILE__)
+require File.expand_path('../shared/shared', __FILE__)
+
+describe 'QuickTreeView' do
+  #let(:widget) { browser.quick_window(:name, "Bookmarks Panel Window").quick_treeview(:name, "Bookmarks View") }
+  #subject { widget }
+  #it_behaves_like 'a widget'
+  
+  #its(:type) { should == :treeview }
+
+  #describe '#correct_type?' do
+  #end
+
+  
+end
