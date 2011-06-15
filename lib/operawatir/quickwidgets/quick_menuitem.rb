27f0a4d99202c9c0a1963699011355ccc4e91dbe
Karianne Ekern <karie@opera.com>
2011-06-15 15:02:19 +0200

    Added QuickMenuItem#checked?

--- lib/operawatir/quickwidgets/quick_menuitem.rb
+++ lib/operawatir/quickwidgets/quick_menuitem.rb
@@ -71,6 +71,14 @@ module OperaWatir
     def selected?
       element.isSelected()
     end
+    
+    ######################################################################
+    #
+    # @return true if element is a checkbox item that is checked, else false
+    #
+    def checked?
+      element.isChecked()
+    end
      
     #####################################################################
     #
